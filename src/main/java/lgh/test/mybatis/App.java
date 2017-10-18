fcb515aa41a4891ee70d83ac5987015399781509
JohnsonLau <gatement@gmail.com>
2017-10-18 18:05:49 +0800

    go

--- src/main/java/lgh/test/mybatis/App.java
+++ src/main/java/lgh/test/mybatis/App.java
@@ -1,9 +1,15 @@
 package lgh.test.mybatis;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class App {
+	public static void main(String[] args) throws Exception {
+		String resource = "lgh/test/mybatis/mybatis-config.xml";
+		InputStream inputStream = Resources.getResourceAsStream(resource);
+		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+	}
 }
